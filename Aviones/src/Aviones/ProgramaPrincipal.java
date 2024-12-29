e6c4fe2bb8e55e1292f323059e998b6b2ea00d6b
Zenialuz <zenialcm@gmail.com>
2024-12-29 06:09:59 -0500

    Se solicita los datos del avión al usuario por consola

--- Aviones/src/Aviones/ProgramaPrincipal.java
+++ Aviones/src/Aviones/ProgramaPrincipal.java
@@ -1,12 +1,14 @@
 package Aviones;
 
+import java.util.Scanner;
+
 public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
-		
+						
 		//Creando los 2 aviones, usando el constructor de la clase Avion.
-		Avion avion1 = new Avion("AirbusA350", 32, 3890, "12/12/2024", "David Galvez de Canto");
-		Avion avion2 = new Avion("AirbusA380", 41, 4890, "26/11/2024", "Zenia Canto");
+		Avion avion1 = pedirDatosAvion();
+		Avion avion2 = pedirDatosAvion();
 		
 		//Imprimiendo por consola la cabecera de los datos de los aviones.
 		System.out.println( "Código \t Asientos\tKm \t Revisión \t Responsable"); 
@@ -15,6 +17,27 @@ public class ProgramaPrincipal {
 		System.out.println(avion1.toString());
 		System.out.println(avion2.toString());
 		
+	}
+	
+	public static Avion pedirDatosAvion(){
+		Avion avion;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println( "Ingrese los siguientes datos del avion:");
+		System.out.println( "Código:");
+		String codigo = sc.nextLine();
+		System.out.println( "Asientos disponibles:");
+		int asientos = Integer.parseInt(sc.nextLine());
+		System.out.println( "Km de autonomía:");
+		double km = Double.parseDouble(sc.nextLine());
+		System.out.println( "Fecha de revisión:");
+		String fechaRevision = sc.nextLine();
+		System.out.println( "Responsable de reparación:");
+		String responsable = sc.nextLine();
+		
+		avion = new Avion(codigo, asientos, km, fechaRevision, responsable);
+			
+		return avion;	
 	}
 
 }
